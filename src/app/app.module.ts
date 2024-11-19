689edd50108e3be7324bb0c96f142735127c4c65
juan54456 <azizdhaya26@gmail.com>
2024-11-19 14:40:08 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ResidenceComponent } from './residence/residence.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ResidenceComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/residence/residence.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {Residence} from "../models/models_Residence";
+
+@Component({
+  selector: 'app-residence',
+  templateUrl: './residence.component.html',
+  styleUrls: ['./residence.component.css']
+})
+export class ResidenceComponent {
+  listResidences: Residence[] = [
+    { id: 1, name: 'El fel', address: 'Borj Cedria', image: '../../assets/images/R1.jpeg', status: 'Disponible' },
+    { id: 2, name: 'El yasmine', address: 'Ezzahra', image: '../../assets/images/R2.jpg', status: 'Disponible' },
+    { id: 3, name: 'El Arij', address: 'Rades', image: '../../assets/images/R3.jpg', status: 'Vendu' },
+    { id: 4, name: 'El Anber', address: 'inconnu', image: '../../assets/images/R4.jpg', status: 'En Construction' }
+  ];
+
+  favorites: Residence[] = [];
+  filterText: string = '';
+
+  showLocation(address: string) {
+    if (address === 'inconnu') {
+      alert('L’adresse de cette résidence est inconnue.');
+    } else {
+      alert(`Adresse : ${address}`);
+    }
+  }
+
+  likeResidence(residence: Residence) {
+    if (!this.favorites.includes(residence)) {
+      this.favorites.push(residence);
+      alert(`${residence.name} ajoutée aux favoris !`);
+    } else {
+      alert(`${residence.name} est déjà dans vos favoris.`);
+    }
+  }
+
+  get filteredResidences() {
+    return this.listResidences.filter(residence =>
+      residence.address.toLowerCase().includes(this.filterText.toLowerCase())
+    );
+  }
+}
